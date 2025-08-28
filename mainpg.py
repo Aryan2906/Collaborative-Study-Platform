d396f64fd4c6bc07bfe2ddebebf4f580a386f07a
Aryan Tiwari <73476932+Aryan2906@users.noreply.github.com>
2025-08-28 21:30:09 +0530

    Deleted the Venv Files

--- mainpg.py
+++ mainpg.py
@@ -1,5 +1,6 @@
 from flask import Flask
+from main import app as application
 app = Flask(__name__)
 @app.route("/")
 def hello_world():
-    return "<p>Hello World!</p>"
+    return "<p>Hello Chicago!</p>"
